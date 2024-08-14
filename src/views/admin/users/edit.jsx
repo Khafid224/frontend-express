cf5d44a51b47260f99b0c7a19f68ed91cce02872
Khafid224 <abdulhafid1230@gmail.com>
2024-08-14 11:05:59 +0700

    Membuat Proses Edit dan Update Data User

--- src/views/admin/users/edit.jsx
+++ src/views/admin/users/edit.jsx
@@ -1,7 +1,80 @@
+//import useState dan useEffect
+import { useState, useEffect } from 'react';
+
 //import SidebarMenu
 import SidebarMenu from '../../../components/SidebarMenu';
 
+//import useNavigate and useParams
+import { useNavigate, useParams } from 'react-router-dom';
+
+//import js cookie
+import Cookies from 'js-cookie';
+
+//import api
+import api from '../../../services/api';
+
 export default function UsersEdit() {
+
+    //useNavigate
+    const navigate = useNavigate();
+
+    //destruct ID
+    const { id } = useParams();
+
+    //define state
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    //state validation
+    const [validation, setValidation] = useState([]);
+
+    //method fetchDetailUser
+    const fetchDetailUser = async () => {
+        try {
+            const response = await api.get(`/api/admin/users/${id}`);
+            //assign to state
+            setName(response.data.data.name);
+            setEmail(response.data.data.email);
+        } catch (error) {
+            console.error("There was an error fetching the user details!", error);
+        }
+    }
+
+    //hook useEffect
+    useEffect(() => {
+        //call method "fetchDetailUser"
+        fetchDetailUser();
+    }, [id]);
+
+    //method "updateUser"
+    const updateUser = async (e) => {
+        e.preventDefault();
+
+        //get token from cookies
+        const token = Cookies.get('token');
+
+        if (token) {
+            //set authorization header with token
+            api.defaults.headers.common['Authorization'] = token;
+
+            try {
+                await api.put(`/api/admin/users/${id}`, {
+                    name: name,
+                    email: email,
+                    password: password
+                });
+
+                //redirect ke halaman users
+                navigate('/admin/users');
+            } catch (error) {
+                //assign error to state validation
+                setValidation(error.response.data);
+            }
+        } else {
+            console.error("Token is not available!");
+        }
+    }
 
     return (
         <div className="container mt-5 mb-5">
@@ -11,16 +84,59 @@ export default function UsersEdit() {
                 </div>
                 <div className="col-md-9">
                     <div className="card border-0 rounded shadow-sm">
-                        <div class="card-header">
-                            ADD USER
+                        <div className="card-header">
+                            EDIT USER
                         </div>
                         <div className="card-body">
-                            HALAMAN USER EDIT
+                            {validation.errors && (
+                                <div className="alert alert-danger mt-2 pb-0">
+                                    {validation.errors.map((error, index) => (
+                                        <p key={index}>{error.path} : {error.msg}</p>
+                                    ))}
+                                </div>
+                            )}
+                            <form onSubmit={updateUser}>
+                                <div className="form-group mb-3">
+                                    <label className="mb-1 fw-bold">Full Name</label>
+                                    <input
+                                        type="text"
+                                        value={name}
+                                        onChange={(e) => setName(e.target.value)}
+                                        className="form-control"
+                                        placeholder="Full Name"
+                                    />
+                                </div>
+
+                                <div className="form-group mb-3">
+                                    <label className="mb-1 fw-bold">Email address</label>
+                                    <input
+                                        type="email"
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                        className="form-control"
+                                        placeholder="Email Address"
+                                    />
+                                </div>
+
+                                <div className="form-group mb-3">
+                                    <label className="mb-1 fw-bold">Password</label>
+                                    <input
+                                        type="password"
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        className="form-control"
+                                        placeholder="Password"
+                                    />
+                                </div>
+
+                                <button type="submit" className="btn btn-sm btn-primary">
+                                    UPDATE
+                                </button>
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    )
-
+    );
 }
